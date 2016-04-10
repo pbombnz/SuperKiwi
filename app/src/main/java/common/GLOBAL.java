6884be7efad3a33d1a5ed3f49ba562655f214e16
Prashant B <prashant_bhikhu@hotmail.com>
2016-04-10 16:21:47 +1200

    Disable DEBUG mode for release

--- app/src/main/java/common/GLOBAL.java
+++ app/src/main/java/common/GLOBAL.java
@@ -3,9 +3,9 @@ package common;
 import java.io.File;
 
 public class GLOBAL {
-    public static final boolean DEBUG = true;
+    public static final boolean DEBUG = false;
 
-    public static boolean isSamsungRom() {
-        return new File("/system/framework/twframework.jar").isFile();
-    }
+    //public static boolean isSamsungRom() {
+    //    return new File("/system/framework/twframework.jar").isFile();
+    //}
 }
