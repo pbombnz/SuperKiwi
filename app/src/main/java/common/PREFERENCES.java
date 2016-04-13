8a4660447a65ed795bc2697f9fbeea809d7ffd7e
Prashant B <prashant_bhikhu@hotmail.com>
2016-04-13 18:50:22 +1200

    Default should be true, so if an error in reading the SharedPreferences occurs we can why  possibly.

--- app/src/main/java/common/PREFERENCES.java
+++ app/src/main/java/common/PREFERENCES.java
@@ -26,7 +26,7 @@ public final class PREFERENCES {
 
 
         public static final class MAIN {
-            public static final boolean DEBUG = false;
+            public static final boolean DEBUG = true;
         }
     }
 
