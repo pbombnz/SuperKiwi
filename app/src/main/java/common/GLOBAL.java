19e1eb35d7cafdaa69950080e03423bc15edfae4
Prashant B <prashant_bhikhu@hotmail.com>
2016-04-17 22:44:27 +1200

    Changed Debug mode default

--- app/src/main/java/common/GLOBAL.java
+++ app/src/main/java/common/GLOBAL.java
@@ -3,7 +3,7 @@ package common;
 import java.io.File;
 
 public class GLOBAL {
-    public static final boolean DEBUG = true;
+    public static final boolean DEBUG = false;
 
     //public static boolean isSamsungRom() {
     //    return new File("/system/framework/twframework.jar").isFile();
