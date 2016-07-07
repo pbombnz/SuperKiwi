5acfbae0a032dd93fd9d558c037b0abe2b3dc711
Prashant B <prashant_bhikhu@hotmail.com>
2016-07-08 02:02:07 +1200

    Removed random commenting as it was only used to teach my friend how to use GitHub

--- app/src/main/java/common/GLOBAL.java
+++ app/src/main/java/common/GLOBAL.java
@@ -3,10 +3,5 @@ package common;
 import java.io.File;
 
 public class GLOBAL {
-    //hgjgiug
     public static final boolean DEBUG = false;
-
-    //public static boolean isSamsungRom() {
-    //    return new File("/system/framework/twframework.jar").isFile();
-    //} hi
 }
